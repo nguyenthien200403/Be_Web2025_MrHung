aac674833ab04aeb5f49e45a385ec2e27e9a7a5c
nguyenthien200403 <nguyenngocthien200403@gmail.com>
2025-04-03 11:11:37 +0700

    source main for back-end web booking ticket movies

---
+++ be_MrHung/src/test/java/com/example/be_MrHung/BeMrHungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_MrHung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMrHungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
